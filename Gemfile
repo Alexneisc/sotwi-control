38e3cd14b0a315db43d2dc76ef0b5a5662019205
Alexey Neischenko <alexneisc@gmail.com>
2017-08-29 21:24:51 +0300

    Configure deploy with Capistrano

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development do
   gem 'capistrano-rails', require: false
   gem 'capistrano-bundler', require: false
   gem 'capistrano3-puma', require: false
+  gem 'capistrano-upload-config'
   gem 'activerecord_sane_schema_dumper'
   gem 'capistrano-template', require: false
 end
---
+++ config/deploy.rb
@@ -0,0 +1,73 @@
+# config valid only for current version of Capistrano
+lock "3.9.0"
+
+set :application, 'sotwi-control'
+set :user, 'deploy'
+set :repo_url, 'git@bitbucket.org:alexneisc/sotwi-control.git'
+set :rvm_ruby_version, '2.4.1@sotwi-control'
+
+append :linked_files, 'config/secrets.yml', 'config/database.yml', 'puma.rb'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/assets'
+
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :ssh_options,     { forward_agent: true, user: fetch(:user) }
+
+set :puma_role, :web
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, 30
+set :puma_threads, [5, 5]
+set :puma_workers, 1
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :nginx_config_name, 'sotwi-control'
+set :puma_conf, "#{shared_path}/config/puma.rb"
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before 'puma:start', :make_dirs
+end
+
+namespace :deploy do
+  before 'check:linked_files', 'puma:config'
+  before 'check:linked_files', 'puma:nginx_config'
+end
+
+namespace :deploy do
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts 'WARNING: HEAD is not the same as origin/master'
+        puts 'Run `git push` to sync changes.'
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
